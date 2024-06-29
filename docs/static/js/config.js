6a542f7240ea1a3b7446f82bdbd0a4cf9792b033
HuangJyunYu <yillkid@gmail.com>
2024-06-30 04:00:28 +0800

    Update config.js

--- docs/static/js/config.js
+++ docs/static/js/config.js
@@ -1,3 +1,2 @@
-// Host
-HOST_URL_EID_DAEMON = "https://eid-backend.townway.com.tw";
-HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.townway.com.tw";
+HOST_URL_EID_DAEMON = "https://eid-backend.4impact.cc";
+HOST_URL_TPLANET_DAEMON = "https://tplanet-backend.4impact.cc";
